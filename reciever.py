238f6047b10929f1dc5b6dc7a3f43b298d111d4e
DearMordor <dearmordor@gmail.com>
2021-04-01 22:59:51 +0600

    Improved code. Fixed bugs with last hash code packet.

--- reciever.py
+++ reciever.py
@@ -2,22 +2,37 @@ import hashlib
 import socket
 import zlib
 
-localIP = "192.168.30.38"
+localIP = "192.168.30.15"
 targetIP = "192.168.30.38"
 
 localPort = 4023
 targetPort = 7110
 
 bufferSize = 1024
+CHARACTER = 124
+TIMEOUT = 5
 
 
 def get_hash_code(data):
     return data.decode()
 
 
+def while_loop_for_data_parse(data, index, number):
+    while data[index] != CHARACTER:
+        number += chr(data[index])
+        index += 1
+
+    return data, index, number
+
+
+def increase_index(index):
+    index += 1
+    return index
+
+
 class Server:
     """
-    UDP server for PSIA.
+    UDP server for Computer Networks' class.
     author: Nurkozhin Arlan
     """
 
@@ -29,10 +44,11 @@ class Server:
         self.udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.count = 1
         self.all_data = b''
-        self.amount_of_packages = -1
+        self.amount_of_packages = 0
         self.counter_from_sender = 0
         self.crc32 = 0
         self.hash_code = hashlib.md5()
+        self.isEnd = False
 
     def bind_server(self):
         """Binds server with sender via sockets"""
@@ -43,21 +59,12 @@ class Server:
     def check_packages(self):
         """Checks a current count from sender and compares with local count"""
         if self.counter_from_sender == self.count:
-            print("Counters jsou v poho")
-            # self.send_success()
-            # self.send_bytes("1".encode())
-            # print("1 was sent to the client\n")
-            self.count += 1
+            print("Counters are the same")
             return 1
         else:
             if self.counter_from_sender < self.count:
-                # self.send_success()
-                # self.send_bytes("1".encode())
-                print("0 was sent to the client")
                 return 0
-            # self.send_error()
-            # self.send_bytes("0".encode())
-            # print("0 was sent to the client\n")
+
             if self.count < 0:
                 self.count = 0
 
@@ -65,66 +72,75 @@ class Server:
 
     def listener(self):
         """Main function listens to data from sender"""
-        # f = None
-        while True:
-            data, addr = self.udp_server_socket.recvfrom(self.buffer_size)
-            print(data)
-            print('received from: ', addr, 'data: package_' + str(self.count))
+        while not self.isEnd:
+            data = self.get_data_and_addr_from_sender()
             data = self.parse_data(data)
+
             if data is None:
                 self.send_error()
                 continue
 
-            if self.count == 1:
+            if self.count == 1:  # At first iteration we get the filename. If not, continue ask receiver
                 self.get_filename(data)
 
                 if not self.check_packages_and_crc(data):
                     print("Iteration was continued ")
+                    self.send_success_or_error()
                     continue
+                else:
+                    self.send_success()
+                    self.count += 1
 
-                # f = open(self.filename, "wb")
-
-            elif self.count == 2:  # For future, check if data is number
+            elif self.count == 2:  # At second we do the same, but with file's size ( amount of packages)
                 self.get_size(data)
 
                 if not self.check_packages_and_crc(data):
                     print("Iteration was continued ")
+                    self.send_success_or_error()
                     continue
+                else:
+                    self.send_success()
+                    self.count += 1
             else:
                 print("Self count " + str(self.count) + " Sender's amount_of_packages " + str(self.amount_of_packages))
-                if self.count == self.amount_of_packages:
-                    if self.check_packages_and_crc(data):
-                        # print(data)
-                        hash_from_sender = get_hash_code(data)
-                        print("Hash from sender: " + hash_from_sender)
-                        print("My hash " + str(self.hash_code.hexdigest()))
-                        if self.compare_hash_codes(hash_from_sender):
-                            print("Hash codes are equal!")
+                if self.count == self.amount_of_packages and self.check_packages_and_crc(data):
+                    hash_from_sender = get_hash_code(data)
 
-                        break
+                    print("Hash from sender: " + hash_from_sender)
+                    print("My hash " + str(self.hash_code.hexdigest()))
 
-                # if self.check_packages(self.counter_from_sender) != 0 and self.control_crc(data):
-                if self.check_packages_and_crc(data):
-                    print("All data was concatenated with just data\n")
-                    # if self.count == 1000:  # 1000 is number of paket. It implemented to not make string too big
-                    #     f.write(self.all_data)
-                    #     self.all_data = b''
-                    self.hash_code.update(data)
-                    self.all_data += data
+                    self.compare_hash_codes(hash_from_sender, data)
+
+                self.concatenate_data_update_hash(data)
+                # if self.check_packages_and_crc(data) and self.count < self.amount_of_packages:
+                #     print("All data was concatenated with just data\n")
+                #     # if self.count == 1000:  # 1000 is number of paket. It implemented to not make string too big
+                #     #     f.write(self.all_data)
+                #     #     self.all_data = b''
+                #     self.hash_code.update(data)
+                #     self.all_data += data
+                #     self.send_success()
+                #     if self.count <= self.amount_of_packages:
+                #         self.count += 1
+                # else:
+                #     self.send_success_or_error()
 
         # f.close()
         print(self.filename + " was created!")
 
     def get_filename(self, data):
-        self.filename = data.decode("utf-8")
-        print("Filename got success: " + self.filename)
+        try:
+            self.filename = data.decode("utf-8")
+            print("Filename got success: " + self.filename)
+        except Exception as e:
+            print("Error with filename decoding! ", e)
 
     def get_size(self, data):
         try:
             self.amount_of_packages = int(data.decode("utf-8"))
             print("Size got success: " + str(self.amount_of_packages))
-        except Exception:
-            print("Invalid literal for get_size")
+        except Exception as e:
+            print("Invalid data for get_size function.", e)
 
     def make_file(self):
         with open("out/" + self.filename, "wb") as f:
@@ -138,12 +154,12 @@ class Server:
         self.udp_server_socket.sendto(data, (targetIP, targetPort))
 
     def send_error(self):
-        msg = str(zlib.crc32(b"BAD")) + "|" + "BAD"
+        msg = str(self.counter_from_sender) + "|" + "BAD"
         self.send_bytes(msg.encode())
         print("Error was sent to the client\n")
 
     def send_success(self):
-        msg = str(zlib.crc32(b"GOOD")) + "|" + "GOOD"
+        msg = str(self.counter_from_sender) + "|" + "GOOD"
         self.send_bytes(msg.encode())
         print("Success was sent to the client\n")
 
@@ -151,65 +167,102 @@ class Server:
         """Reads input data up to char '|' then return data without chars before the char '|' """
         try:
             data = self.get_package_counter_from_sender(data)
-
             return self.get_src_from_sender(data)
-        except Exception:
-            print("Error with parsing data")
+        except Exception as e:
+            print("Error with parsing data ", e)
             return None
 
     def control_crc(self, data):
         try:
             self.crc32 = int(self.crc32)
-            if self.crc32 == zlib.crc32(data):
-                print("Crc je v poho")
             return self.crc32 == zlib.crc32(data)
-        except Exception:
-            print("Error with crc!")
+        except Exception as e:
+            print("Error with crc!", e)
             return False
 
     def get_package_counter_from_sender(self, data):
-        i = 0
+        index = 0
         num = ""
-        while data[i] != 124:  # 124 means in ASCII "|"
-            num += chr(data[i])
-            i += 1
 
+        data, index, num = while_loop_for_data_parse(data, index, num)
         self.counter_from_sender = int(num)
-        i += 1
-        return data[i:]
+
+        index = increase_index(index)
+
+        return data[index:]
 
     def get_src_from_sender(self, data):
-        i = 0
+        index = 0
         crc = ""
-        while data[i] != 124:  # 124 means in ASCII "|"
-            crc += chr(data[i])
-            i += 1
-        i += 1
-        self.crc32 = int(crc)
-        return data[i:]
 
-    def compare_hash_codes(self, hash_code_from_sender):
-        return hash_code_from_sender == self.hash_code.hexdigest()
+        data, index, crc = while_loop_for_data_parse(data, index, crc)
+
+        self.crc32 = int(crc)
+        index = increase_index(index)
+
+        return data[index:]
+
+    def compare_hash_codes(self, hash_code_from_sender, data):
+        if self.count == self.amount_of_packages and self.check_packages_and_crc(data):
+            hash_from_sender = get_hash_code(data)
+
+            print("Hash from sender: " + hash_from_sender)
+            print("My hash " + str(self.hash_code.hexdigest()))
+
+            if hash_code_from_sender == self.hash_code.hexdigest():
+                print("Hash codes are equal!")
+                self.send_success()
+                self.count += 1
+            else:
+                self.send_error()
 
     def check_packages_and_crc(self, data):
 
         if self.check_packages() == 1 and self.control_crc(data):
-            print("WTF ARE YOU SENDING")
-            self.send_success()
             return True
         else:
-            print("Counter from sender is " + str(self.counter_from_sender))
-            print("My counter is " + str(self.count))
+
             if self.counter_from_sender < self.count:
-                self.send_success()
                 return False
-            # else:
-            #     self.send_error()
-            #     return False
-            # if self.counter_from_sender < self.count:
-            #     self.count -= 1
-            self.send_error()
+
             return False
+
+    def send_success_or_error(self):
+        if self.counter_from_sender < self.count:
+            self.send_success()
+        else:
+            self.send_error()
+
+    def get_data_and_addr_from_sender(self):
+        if self.count >= self.amount_of_packages:
+            try:
+                data, addr = self.udp_server_socket.recvfrom(self.buffer_size)
+                self.udp_server_socket.settimeout(TIMEOUT)
+                print('received from: ', addr, 'data: package_' + str(self.count))
+                return data
+            except socket.timeout:
+                print("Confirmation timeout")
+                self.isEnd = True
+
+        else:
+            data, addr = self.udp_server_socket.recvfrom(self.buffer_size)
+
+            print('received from: ', addr, 'data: package_' + str(self.count))
+            return data
+
+    def concatenate_data_update_hash(self, data):
+        if self.check_packages_and_crc(data) and self.count < self.amount_of_packages:
+            print("All data was concatenated with just data\n")
+            # if self.count == 1000:  # 1000 is number of paket. It implemented to not make string too big
+            #     f.write(self.all_data)
+            #     self.all_data = b''
+            self.hash_code.update(data)
+            self.all_data += data
+            self.send_success()
+            if self.count <= self.amount_of_packages:
+                self.count += 1
+        else:
+            self.send_success_or_error()
 
 
 # Start

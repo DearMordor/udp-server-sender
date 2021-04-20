54ca2b4a503dfc478bf4856e0ec3690822ece1a3
Kyzr, Ondřej <kyzrondr@fel.cvut.cz>
2021-04-20 22:14:03 +0200

    Updated sender for the 4th task.

--- sender.py
+++ sender.py
@@ -9,42 +9,57 @@ BAD = b"BAD"
 GOOD = b"GOOD"
 SEPARATOR = '|'
 BUFFER_LEN = 1012   # The length of the packet varies from 1023 to 1024
-TIMEOUT = 0.3
+TIMEOUT = 0.2
 DEFAULT_RESPONSE_LEN = 30
-WINDOW_SIZE = 100
+WINDOW_SIZE = 10
 
 # NetDerper
-# TARGET_IP = "127.0.0.1"
-# LOCAL_IP = "127.0.0.1"
-#
-# TARGET_PORT = 4024
-# LOCAL_PORT = 4025
+TARGET_IP = "127.0.0.1"
+LOCAL_IP = "127.0.0.1"
+
+TARGET_PORT = 4024
+LOCAL_PORT = 4025
 
 # Normal
-TARGET_IP = "192.168.30.20"
-LOCAL_IP = "192.168.30.20"
-
-TARGET_PORT = 4023
-LOCAL_PORT = 7110
+# TARGET_IP = "192.168.30.14"
+# LOCAL_IP = "192.168.30.20"
+#
+# TARGET_PORT = 4023
+# LOCAL_PORT = 7110
 
 SOCK = socket(family=AF_INET, type=SOCK_DGRAM)
 SOCK.bind((LOCAL_IP, LOCAL_PORT))
 SOCK.settimeout(TIMEOUT)
 
 
-def check_answer(num, acknowledgement, packet_counter):
+def check_answer(num, crc, acknowledgement, packet_counter):
     """
     Compares the received data with GOOD.
     """
-    if num != packet_counter:
-        print(''.join(["Received old confirmation for packet ", str(num), ", waiting for another."]))
-        return get_answer(packet_counter)
-    if len(acknowledgement) == len(GOOD):
-        print("Received GOOD for " + str(num) + ".")
-        return GOOD
+    if int(crc) == crc32(str(num).encode()):
+        if len(acknowledgement) == len(GOOD):
+            print("Received GOOD for packet " + str(num) + ".")
+            return num, GOOD
 
-    print(''.join(["Received corrupted data or BAD (", str(acknowledgement), "), sending packet again."]))
-    return BAD
+        print("Received BAD for packet " + str(num))
+        return num, BAD
+
+    print("Received corrupted packet number (crc32 wrong) " + str(int(num)) + ".")
+    return packet_counter, BAD
+
+
+def get_separator_index(answer):
+    """
+    Returns the indexes of separators in a confirmation packet.
+    """
+    iterator = 0
+    separators = []
+    for char in str(answer):
+        if char == SEPARATOR:
+            separators.append(iterator-2)
+        iterator += 1
+
+    return separators
 
 
 def get_answer(packet_counter):
@@ -55,43 +70,50 @@ def get_answer(packet_counter):
     try:
         answer = SOCK.recv(DEFAULT_RESPONSE_LEN)
     except timeout:
-        print("Confirmation timeout, sending packet again.")
-        return BAD
+        print("Confirmation timeout.")
+        return packet_counter, BAD
 
     # Tries to decode data to get the acknowledgement
     try:
-        separator_idx = len(str(packet_counter))
-        num = int(answer[:separator_idx])
-        ack = answer[separator_idx + 1:]
+        separator_idx = get_separator_index(answer)
+        num = int(answer[:separator_idx[0]])
+        crc = answer[separator_idx[0] + 1:separator_idx[1]]
+        ack = answer[separator_idx[1] + 1:]
 
-        return check_answer(num, ack, packet_counter)
+        return check_answer(num, crc, ack, packet_counter)
     except Exception as e:
-        print("Received corrupted data, sending packet again.", e)
-        return BAD
+        print("Received corrupted data (" + str(answer) + ").")
+        return packet_counter, BAD
 
 
-def build_packet(data, packet_counter):
+def build_packet(packet_counter, data):
     """
     Builds a packet with this structure: packet_number SEPARATOR crc32 SEPARATOR data
     """
-    return (str(packet_counter) + SEPARATOR + str(crc32(data)) + SEPARATOR).encode() + data
+    return (str(packet_counter) + SEPARATOR + str(crc32(str(packet_counter).encode() + data)) + SEPARATOR).encode() + data
 
 
-def send_bytes(buffer, packet_counter):
+def send_bytes(buffer):
     """
     Sends the data with packet number and divider to TARGET_IP and TARGET_PORT.
     """
-    recv_bad_buffer = []
+    answers = {}
 
     print("Sending " + str(len(buffer)) + " packets:")
-    for i in range(len(buffer)):
-        print(''.join(["Sending data (packet number: ", str(int(packet_counter - len(buffer) + i + 1)), ") of size ", str(len(buffer[i])), "."]))
-        SOCK.sendto(build_packet(buffer[i], packet_counter - len(buffer) + i + 1), (TARGET_IP, TARGET_PORT))
-        if get_answer(packet_counter - len(buffer) + i + 1) == BAD:
-            recv_bad_buffer.append(buffer[i])
+    for packet_num in buffer.keys():
+        print("Sending data (packet number:", str(packet_num) + ") of size", str(len(buffer[packet_num])) + ".")
+        SOCK.sendto(build_packet(packet_num, buffer[packet_num]), (TARGET_IP, TARGET_PORT))
 
-    print()
-    return recv_bad_buffer
+    while GOOD not in answers.values():
+        for packet_num in buffer.keys():
+            answer = (get_answer(packet_num))
+            answers[answer[0]] = answer[1]
+
+    for packet_num in answers.keys():
+        if answers[packet_num] == GOOD and packet_num in buffer.keys():
+            buffer.pop(packet_num)
+
+    print("------------------------------------------------")
 
 
 def calculate_amount_of_packets(filename, buffer_length):
@@ -107,54 +129,53 @@ def calculate_amount_of_packets(filename, buffer_length):
     return number
 
 
-def send_file_info(file_name, packet_counter, buffer_lenght):
-    """
-    Sends the necessary file information.
-    -> file name
-    -> number of packets (with predetermined buffer length 1012)
-    """
-    n_packets = calculate_amount_of_packets(file_name, buffer_lenght)
-    print("Total packets to send " + str(n_packets) + ".")
-
-    send_bytes([file_name[4:].encode()], packet_counter)  # Converts (encodes) the filename to binary
-    send_bytes([str(n_packets).encode()], packet_counter + 1)  # Converts the n_packets to binary
-
-
 def send_file(file_name, buffer_length):
     """
     Opens and sends a file with given buffer length.
     """
     packet_counter = 1
-    buffer_window = []
+    buffer_window = {}
     hash_code = md5()
 
     with open(file_name, "rb") as f:
-        print("Sending file " + file_name + " to address " + TARGET_IP + ":" + str(TARGET_PORT))
 
-        send_file_info(file_name, packet_counter, buffer_length)  # Sends information about file
-        packet_counter += 2  # +2 for the filename and file size
+        # Adds filename packet to the buffer
+        print("Sending file " + file_name + " to address " + TARGET_IP + ":" + str(TARGET_PORT))
+        buffer_window[packet_counter] = file_name[4:].encode()
+        packet_counter += 1
+
+        # Adds number of packets to the buffer
+        n_packets = calculate_amount_of_packets(file_name, buffer_length)
+        print("Total packets to send " + str(n_packets) + ".")
+        buffer_window[packet_counter] = str(n_packets).encode()
+        packet_counter += 1
+
         buffer = f.read(buffer_length - len(str(packet_counter)))
 
-        while buffer != b"":
-            hash_code.update(buffer)
-            buffer_window.append(buffer)
+        while len(buffer_window) != 0:
+            if len(buffer_window) >= WINDOW_SIZE or buffer == b"":
+                send_bytes(buffer_window)
+                if len(buffer_window) == WINDOW_SIZE:
+                    continue
 
-            if len(buffer_window) == WINDOW_SIZE:
-                buffer_window = send_bytes(buffer_window, packet_counter)
+            if buffer != b"":
+                hash_code.update(buffer)
+                buffer_window[packet_counter] = buffer
 
             packet_counter += 1
             buffer = f.read(buffer_length - len(str(packet_counter)))
-        if len(buffer_window) > 0:
-            send_bytes(buffer_window, packet_counter - 1)
-        print("\nSending Hash: " + str(hash_code.hexdigest()))
-        send_bytes([str(hash_code.hexdigest()).encode()], packet_counter)
+
+            if packet_counter == n_packets:
+                print("\nAdding Hash: " + str(hash_code.hexdigest()))
+                buffer_window[packet_counter] = str(hash_code.hexdigest()).encode()
+
         print("\nFile has been sent!")
 
 
 if __name__ == "__main__":
-    # file = "inp/small.bmp"
+    file = "inp/small.bmp"
     # file = "inp/sample_640×426.bmp"
-    file = "inp/test1.bmp"
+    # file = "inp/test1.bmp"
     # file = "inp/udp-packet.png"
     # file = "inp/test2.txt"
     # file = "inp/BIG.bmp"
